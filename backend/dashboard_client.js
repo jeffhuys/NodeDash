f351f3b79182d000f973c2c5299d9fca0531c8c5
vagrant <jeffhuys@gmail.com>
2014-02-10 21:06:07 +0000

    Made start to converting into package form

--- backend/dashboard_client.js
+++ backend/dashboard_client.js
@@ -6,42 +6,10 @@ This file should be run on multiple hosts, which will redirect this servers'
 data to the master server using socket.io
 */
 
-var io = require('socket.io-client');
-var socket = io.connect('cube.writebrite.nl', {
-    port: 6278
+var nodedash = require('node-dash-client');
+
+nodedash.setDebug(true);
+
+nodedash.connect("wheatley.writebrite.nl", 6278, "", function() {
+	nodedash.sendStatsAutomatically(10000);
 });
-
-var os = require('os');
-var osutils = require('os-utils');
-var hostname = os.hostname();
-
-console.log("[DASHCLIENT] Starting dashboard client...");
-console.log("[DASHCLIENT] Hostname: " + hostname);
-
-var loop = function() {
-	osutils.cpuUsage(function(cpu) {
-		cpu = Math.round(cpu * 100);
-		socket.emit('stats', { 
-			hostname: hostname, 
-			cpu: cpu, 
-			freemem: osutils.freemem(), 
-			totalmem: osutils.totalmem(), 
-			active_processes: "5"
-		});
-		console.log("[DASHCLIENT] Sending stats... " + cpu + " " + osutils.freemem());
-		setTimeout(loop, 4000);
-	});
-		
-};
-
-socket.on('connect', function () { 
-	console.log("[DASHCLIENT] Socket connected"); 
-	socket.on('identify', function() {
-		socket.emit('identify', { what: "server", hostname: hostname });
-	});
-
-	loop();
-});
-
-
-
--- frontend/data/controllers.js
+++ frontend/data/controllers.js
@@ -6,7 +6,7 @@ nodedashApp.controller('RootController', ['$scope', 'breadcrumbs', function($sco
 	$scope.breadcrumbs = breadcrumbs;
 
 	$scope.config = {};
-	$scope.config.socketAddress = 'cube.writebrite.nl:6278';
+	$scope.config.socketAddress = 'wheatley.writebrite.nl:6278';
 
 	$scope.reconnect = function() {
 		if($scope.socket) {
@@ -22,18 +22,16 @@ nodedashApp.controller('RootController', ['$scope', 'breadcrumbs', function($sco
 
 		console.log($scope.config.socketAddress);
 
-	  	$scope.socket = io.connect($scope.config.socketAddress, {'force new connection':true});
+		$scope.socket = io.connect($scope.config.socketAddress, {'force new connection':true});
 
-	  	$scope.socket.on('identify', function() {
-	  		$scope.socket.emit('identify', {what: "client"});
-	  	});
+		$scope.socket.on('identify', function() {
+			$scope.socket.emit('identify', {what: "client"});
+		});
 
-	  	$scope.socket.on('stats', function(data) {
+		$scope.socket.on('stats', function(data) {
 			// Check if the server is already being tracked.
 			// If true, we will change the data already available.
 			// If false, we will append the data to the array.
-			//console.dir(data);
-
 			var exists = false;
 			var index = -1;
 			for (var i = 0; i < $scope.servers.length; i++) {
@@ -41,7 +39,7 @@ nodedashApp.controller('RootController', ['$scope', 'breadcrumbs', function($sco
 					exists = true;
 					index = i;
 				}
-			};
+			}
 
 			if(!exists) {
 				var pushData = {};
@@ -70,76 +68,40 @@ nodedashApp.controller('RootController', ['$scope', 'breadcrumbs', function($sco
 					];
 
 				console.log($scope.servers[newindex].chart[0].values);
-				//$scope.$apply();
 
-				//console.log("Applied");
-
-				//console.table($scope.servers);
 			} else {
 				$scope.servers[index].data = data;
-				//console.log($scope.servers[index]);
 				$scope.servers[index].healthy = true;
 				$scope.servers[index].lastseen = Math.round(new Date().getTime() / 1000);
+
+				$scope.servers[index].chart[0].values.push([ new Date().getTime(), Math.round(100-($scope.servers[index].data.stats.freemem/$scope.servers[index].data.stats.totalmem) * 100) ] );
+				$scope.servers[index].chart[1].values.push([ new Date().getTime(), $scope.servers[index].data.stats.cpu]);
+				$scope.servers[index].chart[2].values.push([ new Date().getTime(), $scope.servers[index].data.stats.events_per_second]);
+
+
+				for (var j = 0; j < $scope.servers[index].chart.length; j++) {
+					if($scope.servers[index].chart[j].values.length > 60) {
+						$scope.servers[index].chart[j].values.shift();
+					}
+				}
 				$scope.$apply();
 			}
 		});
 
-	  	$scope.checkServers = function() {
-	  		//console.log("Checking..." + $scope.refreshSeconds);
-	  		for (var i = 0; i < $scope.servers.length; i++) {
-	  			if(Math.round(new Date().getTime() / 1000) - $scope.servers[i].lastseen >= 10) {
-	  				$scope.servers[i].healthy = false;
-
+		$scope.checkServers = function() {
+			for (var i = 0; i < $scope.servers.length; i++) {
+				console.log($scope.servers[i].lastseen);
+				if(Math.round(new Date().getTime() / 1000) - $scope.servers[i].lastseen >= 10) {
+					$scope.servers[i].healthy = false;
 				} else {
 					$scope.servers[i].healthy = true;
 				}
 				
-
-				
-				// if(!$scope.servers[i].chart) {
-				// 	$scope.servers[i].chart = {};
-				// 	$scope.servers[i].chart = [
-				// 	{
-				// 		"key": "Visitors",
-				// 		"values": [ [0, 0] ]
-				// 	}];
-				// }
-
-				if(!$scope.servers[i].chart) {
-
-					//$scope.servers[i].chart[0].values.push([ new Date().getTime(), $scope.servers[i].stats.events_per_second]);
-					console.log("Yes");
-					console.dir($scope.servers.chart[0]);
-				} else {
-					//console.log("No");
-				}
-
-				//if($scope.servers[i].data.stats.events_per_second != undefined) {
-				//	$scope.servers[i].chart[0].values.push([ new Date().getTime(), $scope.servers[i].data.stats.events_per_second]);
-				//} else {
-					
-				//}
-
-				$scope.servers[i].chart[0].values.push([ new Date().getTime(), Math.round(100-($scope.servers[i].data.stats.freemem/$scope.servers[i].data.stats.totalmem) * 100) ] );
-				$scope.servers[i].chart[1].values.push([ new Date().getTime(), $scope.servers[i].data.stats.cpu]);
-				$scope.servers[i].chart[2].values.push([ new Date().getTime(), $scope.servers[i].data.stats.events_per_second]);
-
-
-				for (var j = 0; j < $scope.servers[i].chart.length; j++) {
-					if($scope.servers[i].chart[j].values.length > 60) {
-						$scope.servers[i].chart[j].values.shift();
-					}
-				};
-
-				
-				console.log($scope.servers[i].chart[0].values);
-				//$scope.servers[i].chart[0].values[0] = [0, Math.random() * 100];
-
 				$scope.$apply();
-	  		};
-	  		setTimeout($scope.checkServers, 1000);
-	  	};
-	  	$scope.checkServers();
+			}
+			setTimeout($scope.checkServers, 1000);
+		};
+		$scope.checkServers();
 	};
 	$scope.reconnect();
 
